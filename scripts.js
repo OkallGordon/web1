23dadd899ae03fcd437e36df8aa8de7ed35f98ea
OkallGordon <gordonochieng5454@gmail.com>
2025-02-18 15:35:57 +0300

    modified

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ function toggleTheme() {
     document.body.dataset.theme = 
         document.body.dataset.theme === 'dark' ? 'light' : 'dark';
 }
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
