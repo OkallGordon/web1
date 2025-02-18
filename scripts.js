b2d411e8a819e366e48ebff4525ee130fa5ce361
OkallGordon <gordonochieng5454@gmail.com>
2025-02-18 14:47:57 +0300

    modify

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ function toggleTheme() {
     document.body.dataset.theme = 
         document.body.dataset.theme === 'dark' ? 'light' : 'dark';
 }
+
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
